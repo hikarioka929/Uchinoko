fc629ed5adc29196ccded2fdd99f04f5a02dd289
piyoppa <piyoppa@gmail.com>
2021-02-18 03:26:20 +0900

    start login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+
+  scope module: 'admin' do
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+end
+
+  scope module: 'users' do
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  end
+
+  root "homes#index"
+  get "/about" => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
