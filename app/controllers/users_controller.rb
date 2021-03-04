839b0eb173f6caf7fedaea65ebb329ab820ce659
piyoppa <piyoppa@gmail.com>
2021-03-04 12:30:32 +0900

    start follow view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,9 +37,11 @@ class UsersController < ApplicationController
     end
 
     def followings
+        @user = User.find(params[:id])
     end
 
     def followers
+        @user = User.find(params[:id])
     end
 
     private
