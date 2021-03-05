5f28879bfc5bd9a531e56b17ae0c5611be16c6de
piyoppa <piyoppa@gmail.com>
2021-03-05 16:56:41 +0900

    start user logical delete

--- Gemfile
+++ Gemfile
@@ -59,3 +59,4 @@ gem 'jquery-rails'
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
 gem 'enum_help'
+gem 'paranoia'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_030108) do
+ActiveRecord::Schema.define(version: 2021_03_05_074959) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_030108) do
     t.string "name"
     t.text "introduction"
     t.string "image_id"
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
