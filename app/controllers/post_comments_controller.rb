590dc3ef5d43ec9033aa824d7e9137770d14ce69
piyoppa <piyoppa@gmail.com>
2021-03-15 23:48:46 +0900

    finish asynchronous comment function

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,6 +2,7 @@ class PostCommentsController < ApplicationController
 
     def create
         @post = Post.find(params[:post_id])
+        @post_comments = PostComment.all
         @post_comment = current_user.post_comments.new(post_comment_params)
         @post_comment.post_id = @post.id
         @post_comment.save
